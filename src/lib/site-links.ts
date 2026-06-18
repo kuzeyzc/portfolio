774b26e1434d001b7641044ed213e33f0b710977
Kuzey Yazıcıoğlu <kuzeyzc@gmail.com>
2026-06-18 15:09:09 +0300

    links change

--- src/lib/site-links.ts
+++ src/lib/site-links.ts
@@ -3,8 +3,8 @@ export const SITE_BRAND = "NorthBound Studio";
 
 export const SITE_LINKS = {
   email: "mailto:kuzeyzc@gmail.com",
-  linkedin: "https://linkedin.com/in/northbound",
-  github: "https://github.com/northbound",
+  linkedin: "https://www.linkedin.com/in/kuzey-yazıcıoğlu-45736a3aa/",
+  github: "https://github.com/kuzeyzc",
   behance: "https://behance.net/northbound",
   instagram: "https://www.instagram.com/northbound.std/",
 } as const;
